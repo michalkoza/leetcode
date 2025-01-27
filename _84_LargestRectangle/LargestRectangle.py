2d2a5c6b6e7c4db68eb09cde41cb82a2f0b4e876
Michal Koza <2995498+michalkoza@users.noreply.github.com>
2025-01-27 01:19:15 +0100

    Solve 84 stack

--- _84_LargestRectangle/LargestRectangle.py
+++ _84_LargestRectangle/LargestRectangle.py
@@ -1,31 +1,23 @@
-import bisect
-from collections import defaultdict
 from typing import List
 
 
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
         n = len(heights)
-        rectangles_tracker = defaultdict(int)
-        relevant_heights = []
         biggest_so_far = 0
+        increasing_height_indexes = []
         for idx in range(len(heights)):
-            height = heights[idx]
-            i = bisect.bisect_left(relevant_heights, height)
-            if i >= len(relevant_heights) or relevant_heights[i] != height:
-                bisect.insort(relevant_heights, height)
-            rectangles_tracker[relevant_heights[i]] += 1
-            for j in range(len(relevant_heights) - 1, i, -1):
-                collected = rectangles_tracker[relevant_heights[j]]
-                biggest_so_far = max(biggest_so_far, collected * relevant_heights[j])
-                rectangles_tracker[relevant_heights[j]] = 0
-                rectangles_tracker[relevant_heights[j - 1]] += collected
-            relevant_heights = relevant_heights[:i + 1]
+            while increasing_height_indexes and heights[increasing_height_indexes[-1]] > heights[idx]:
+                height = heights[increasing_height_indexes.pop()]
+                previous = increasing_height_indexes[-1] if increasing_height_indexes else -1
+                width = idx - previous - 1
+                biggest_so_far = max(biggest_so_far, height * width)
+            increasing_height_indexes.append(idx)
 
-        for j in range(len(relevant_heights) - 1, -1, -1):
-            collected = rectangles_tracker[relevant_heights[j]]
-            biggest_so_far = max(biggest_so_far, collected * relevant_heights[j])
-            if j > 0:
-                rectangles_tracker[relevant_heights[j - 1]] += collected
+        while increasing_height_indexes:
+            height = heights[increasing_height_indexes.pop()]
+            previous = increasing_height_indexes[-1] if increasing_height_indexes else -1
+            width = n - previous - 1
+            biggest_so_far = max(biggest_so_far, height * width)
 
         return biggest_so_far
